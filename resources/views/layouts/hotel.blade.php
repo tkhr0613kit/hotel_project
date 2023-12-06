83e7cc015673ebb81c1de1dea49932a9c109da5c
yuki-gnd <job.yg2301@gmail.com>
2023-12-06 15:40:04 +0900

    guestsテーブル作成、view作成中

---
+++ resources/views/layouts/hotel.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>New Hotel Group</title>
+</head>
+<body>
+    <header>
+        <div class="hotel_name">
+            <h1>New Hotel Group</h1>
+        </div>
+        <nav>
+            <ul class="select_menu">
+                <li><a href="guest/index">利用者一覧</a></li>
+                <li><a href="guest/add">利用者登録</a></li>
+                <li><a href="">管理画面</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+        
+    </footer>
+</body>
+</html>
