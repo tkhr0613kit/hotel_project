ff2473f580d9ad4ef278c30e0e7b9bbc12b93a89
munenritr5 <xdxpf329@yahoo.co.jp>
2023-12-11 15:24:05 +0900

    Reservationモデルに追記

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Reservation extends Model
 {
     use HasFactory;
+
+    // 主キーにid以外の名前を付けているので指定
+    protected $primaryKey = 'reservation_id';
+
+    public function guest() {
+        //belongsTo(主テーブルの指定、従テーブルの外部キー、対応する主テーブルの主キー)
+        return $this->belongsTo('App\Guest', 'guest_id','guest_id');
+    }
 }
