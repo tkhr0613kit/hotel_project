c7d120e91d0988a6acda94ad016983f451f79e8b
n-kazunari0820 <n.kazunarii0820@gmail.com>
2023-12-11 15:59:46 +0900

    belongsToMany

---
+++ app/Models/Reservation.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Reservation extends Model
+{
+    use HasFactory;
+
+    public function reserve_lists(){
+       return $this->belongsToMany('Room::class','reserve_lists','reservation_id','room_id');
+    }
+}
