d4bcac069ddf46df6ea81327fe67b7e9cd47fd3a
munenritr5 <xdxpf329@yahoo.co.jp>
2023-12-12 12:57:02 +0900

    モデルに追記

--- resources/views/layouts/hotel.blade.php
+++ resources/views/layouts/hotel.blade.php
@@ -5,31 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-
     <title>APホテル</title>
     <style>
-        body {
-            color: #333333;
-            background-color: #FFFFFF;
-            font-size: 100%;
-            line-height: 1.7;
-            margin: 10px auto;
-            width: 90%;
-            -webkit-text-size-adjust: 100%;
-        }
-        a {
-            text-decoration: none;
-        }
-        .main-nav {
-            display: flex;
-            font-size: 1.25rem;
-            text-transform: uppercase;
-            list-style: none;
-        }
-        .main-nav li {
-            margin-left: 36px;
-
-
         li {
             list-style: none;
         }
@@ -41,27 +18,24 @@
             display: flex;
             justify-content: space-between;
         }
-        .guest_name,.guest_tel,.guest_address {
-            width: 51em;
+        .main-nav li {
+            margin-left: 36px;
         }
     </style>
 </head>
 <body>
     <header>
-
         <h1>APホテル</h1>
         <nav>
-            <ul class="main-nav">
-                <li><a href="/guest">利用者一覧</a></li>
-                <li><a href="/reserve">予約一覧</a></li>
-            </ul>
-            <ul class="main-nav">
-                <li><a href="/guest/add">利用者一覧</a></li>
-                <li><a href="/reserve/add">予約一覧</a></li>
+            <ul class="main_nav">
+                <li><a href="#">ホーム</a></li>
+                <li><a href="#">管理者ログイン</a></li>
+                <li><a href="guest/add">利用者管理</a></li>
+                <li><a href="#">予約管理</a></li>
+                <li><a href="#">部屋管理</a></li>
             </ul>
         </nav>
     </header>
-
     <body>
         @yield('body')
     </body>
