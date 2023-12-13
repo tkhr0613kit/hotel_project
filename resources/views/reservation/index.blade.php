3e4ecc6c954c231c6087844424370e80e02c53b2
n-kazunari0820 <n.kazunarii0820@gmail.com>
2023-12-13 11:01:36 +0900

    中間OK

--- resources/views/reservation/index.blade.php
+++ resources/views/reservation/index.blade.php
@@ -11,6 +11,7 @@
         <th>チェックイン日</th>
         <th>チェックアウト日</th>
         <th>部屋番号</th>
+        <th>部屋の種類</th>
     @foreach($reservations as $reservation)
     <tr>
         <th>{{$reservation->reservation_id}}</th>
@@ -22,7 +23,8 @@
         <td>{{$reservation->check_in_date}}</td>
         <td>{{$reservation->check_out_date}}</td>
 
-        <td>{{$reservation->reserve_lists}}</td>
+        <td>{{$reservation->reserve_lists->first()->room_number}}</td>
+        <td>{{$reservation->reserve_lists->first()->room_type->room_name}}</td>
     </tr>
     @endforeach
     </table>
